411ff9c7fcc9a4bdca288e9fc2b4f8fd074cb456
@higakikeita <higaki.keichan@gmail.com>
2020-03-18 18:31:12 +0900

    アソシエーション定義

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,2 +1,5 @@
 class Reaction < ApplicationRecord
+  belongs_to :to_user, class_name: "User"
+  belongs_to :from_user, class_name: "User"
+  enum status: { like: 0, dislike: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_145820) do
+ActiveRecord::Schema.define(version: 2020_03_18_091625) do
+
+  create_table "reactions", force: :cascade do |t|
+    t.integer "to_user_id", null: false
+    t.integer "from_user_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
