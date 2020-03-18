58b003d9aeaf496c2c37d7dec3aa1afc173d9869
@higakikeita <higaki.keichan@gmail.com>
2020-03-18 17:48:10 +0900

    ダミーデータの挿入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :img_name, ImgNameUploader
+  enum sex: { 男: 0, 女: 1 }
 end
