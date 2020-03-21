353ced9832af14f02d0d7ebd8aeb8fc097ed68b7
@higakikeita <higaki.keichan@gmail.com>
2020-03-21 14:12:41 +0900

    action_cableの実装

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -1,6 +1,7 @@
 class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    
+    stream_for "chat_room_#{params[:room_id]}"
   end
 
   def unsubscribed
@@ -8,5 +9,11 @@ class ChatRoomChannel < ApplicationCable::Channel
   end
 
   def speak
+    message = ChatMessage.create(chat_room_id: data['room_id'], user_id: data['user_id'], message: data['message'])
+    ChatRoomChannel.broadcast_to "chat_room_#{data['room_id']}", content: render_message(message)
   end
+  private
+    def render_message(message)
+      ApplicationController.renderer.render(partial: 'chat_messages/chat_message', locals: { chat_message: message, current_user: message.user })
+    end
 end
