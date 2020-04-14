b21a861ad92a28620af2bce94df31d9a1103e327
@higakikeita <higaki.keichan@gmail.com>
2020-04-14 20:19:09 +0900

    画像の追加

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -13,7 +13,7 @@ class ChatController < ApplicationController
   def show
     chat_room = ChatRoom.find_by(id: params[:id])
     @chat_room_user = chat_room.chat_room_users.
-      where.not(user_id: current_user.id).first.user
+where.not(user_id: current_user.id).first.user
     @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
   end
 end
